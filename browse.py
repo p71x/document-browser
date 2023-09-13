2ef4b80a42fc33c228afecaafcb765e50318fea2
Piotr Chamera <piotr@chamera.eu>
2023-09-13 20:39:59 +0200

    big rearrangement; new class DocumentView encapsulating view parameters

--- browse.py
+++ browse.py
@@ -47,6 +47,140 @@ else:
     config = {}
 
 
+# ------------------------------------------------------------------------------
+# ViewDoc class
+# ------------------------------------------------------------------------------
+
+class DocumentView:
+    """
+    Encapsulate view of document
+
+    file_name = file name of viewed document
+    doc = fitz (PyMuPDF) document instance bound to the view
+    doc_page_count = page count of document
+    page_index = 0 # currently viewed page index (0 based)
+    zoom = scale factor – 1 is natural size (screen calibrated to physical size of document)
+    cache = dict; cache of viewed pages (as pixmaps)
+    max_size = maximal dimensions of viewed page, tuple (width, height)
+    colorspace = fitz.csRGB # colorspace of page rendering
+    """
+    
+    def __init__(self, file_name, page_index=0, max_size=None, colorspace=fitz.csRGB, zoom=1.0 ):
+        self.file_name = file_name
+        self.doc = fitz.open(file_name)
+        self.doc_page_count = len(self.doc)
+        self.page_index = page_index
+        self.zoom = zoom
+        self.cache = dict()
+        self.max_size = max_size
+        self.colorspace = colorspace
+
+    @classmethod
+    def from_config(cls, config_dictionary, max_size=None):
+        """
+        Construct view from data saved in config file.
+        """
+        d = config_dictionary
+        return cls(d['file_name'],
+                   page_index=d['page'],
+                   zoom=d['zoom'],
+                   colorspace=cls._colorspace_from_name(d['colorspace']),
+                   max_size=max_size,
+                   )
+
+    @classmethod
+    def _colorspace_from_name(cls, name):
+        map = {'DeviceGRAY': fitz.csGRAY,
+               'DeviceRGB': fitz.csRGB,
+               }
+        return map[name]
+    
+    def config_dictionary(self):
+        return {"file_name": self.file_name,
+                "page": self.page_index,
+                "zoom": self.zoom,
+                "colorspace": self.colorspace.name,
+                }
+
+    def get_display_list(self):
+        """
+        Return display list of current page (maybe from cache).
+        """
+        cache_key = self.page_index
+        if cache_key in self.cache:
+            return self.cache[cache_key]
+        else:
+            display_list = self.doc[self.page_index].get_displaylist()
+            self.cache[cache_key] = display_list
+            return display_list
+        
+    def get_page_image(self):
+        """
+        Return a tkinter.PhotoImage for a document page index (0-based)
+        """
+        display_list = self.get_display_list()
+        matrix = fitz.Matrix(self.zoom, self.zoom)
+        pixmap = display_list.get_pixmap(matrix=matrix, colorspace=self.colorspace, alpha=False)
+        return pixmap.tobytes("ppm")  # make PPM image from pixmap for tkinter, requires PyMuPDF version > 1.14.5
+
+    def get_fit_zoom(self, fit_size=None):
+        """
+        Return zoom that fits page to available space (self.max_size).
+        """
+        max_size = fit_size or self.max_size
+        rect = self.get_display_list().rect  # the page rectangle
+        zoom = self.zoom
+        if max_size:
+            max_width, max_height = max_size
+            fit_width_zoom = max_width / rect.width
+            fit_height_zoom = max_height / rect.height
+            zoom = min(1, fit_width_zoom, fit_height_zoom)
+            if zoom == 1:
+                zoom = min(fit_width_zoom, fit_height_zoom)
+        return zoom
+
+    def set_zoom(self, zoom):
+        self.zoom = zoom
+        
+    def get_view_title(self):
+        """
+        Compose string for view window title.
+        """
+        return "Page %i of %i from file %s; zoom = %f" % (self.page_index + 1, self.doc_page_count, self.file_name, self.zoom)
+
+    def previous_page(self):
+        """
+        Decrement or wrap around current page index.
+        """
+        previous_page = self.page_index - 1
+        if previous_page < 0:
+            previous_page += self.doc_page_count
+        self.page_index = previous_page
+
+    def next_page(self):
+        """
+        Increment or wrap around current page index.
+        """
+        next_page = self.page_index + 1
+        if next_page >= self.doc_page_count:
+            next_page -= page_count
+        self.page_index = next_page
+   
+
+# ------------------------------------------------------------------------------
+# get physical screen dimension to determine the page image max size
+# ------------------------------------------------------------------------------
+
+# print("screen size = " + str(sg.Window.get_screen_size()))
+w, h = sg.Window.get_screen_size()
+max_width = w - 20
+max_height = h - 55
+max_size = (max_width, max_height)
+
+# ------------------------------------------------------------------------------
+# startup sequence - determine document to open
+# ------------------------------------------------------------------------------
+
 def get_filename_from_GUI():
     fname = sg.PopupGetFile(
         "Select file and filetype to open:",
@@ -65,133 +199,34 @@ def get_filename_from_GUI():
     return fname
 
 if "recent_file" in config:
-    fname = config["recent_file"]["file_name"]
-    cur_page = config["recent_file"]["page"]
+    view = DocumentView.from_config(config["recent_file"], max_size=max_size)
 elif len(sys.argv) == 1:
-    fname = get_filename_from_GUI()
-    cur_page = 0
+    view = DocumentView(get_filename_from_GUI(), page_index=0, max_size=max_size)
+elif len(sys.argv) == 2 and os.path.isfile(sys.argv[1]):
+    view = DocumentView(sys.argv[1], page_index=0, max_size=max_size)
 else:
-    fname = sys.argv[1]
-    cur_page = 0
-
-if not fname:
-    sg.Popup("Cancelling:", "No filename supplied")
-    sys.exit("Cancelled: no filename supplied")
-
-def open_file_to_browse(fname):
-    doc = fitz.open(fname)
-    page_count = len(doc)
-    return doc, page_count
-
-doc, page_count = open_file_to_browse(fname)
-
-# allocate storage for page display lists
-dlist_tab = [None] * page_count
-
-# ------------------------------------------------------------------------------
-# read the page data
-# ------------------------------------------------------------------------------
-def get_page(pno, zoom=False, max_size=None):
-    """Return a tkinter.PhotoImage for a document page number.
-    :arg int pno: 0-based page number
-    :arg zoom: top-left of old clip rect, and one of -1, 0, +1 for dim. x or y
-               to indicate the arrow key pressed
-    :arg max_size: (width, height) of available image area
-    :arg bool first: if True, we cannot use tkinter
-    """
-    dlist = dlist_tab[pno]  # get display list of page number
-    if not dlist:  # create if not yet there
-        dlist_tab[pno] = doc[pno].get_displaylist()
-        dlist = dlist_tab[pno]
-    r = dlist.rect  # the page rectangle
-    clip = r
-    # ensure image fits screen:
-    # exploit, but do not exceed width or height
-    zoom_0 = 1
-    if max_size:
-        zoom_0 = min(1, max_size[0] / r.width, max_size[1] / r.height)
-        if zoom_0 == 1:
-            zoom_0 = min(max_size[0] / r.width, max_size[1] / r.height)
-
-    mat_0 = fitz.Matrix(zoom_0, zoom_0)
-
-    if not zoom:  # show the total page
-        pix = dlist.get_pixmap(matrix=mat_0, alpha=False)
-    else:
-        w2 = r.width / 2  # we need these ...
-        h2 = r.height / 2  # a few times
-        clip = r * 0.5  # clip rect size is a quarter page
-        tl = zoom[0]  # old top-left
-        tl.x += zoom[1] * (w2 / 2)  # adjust topl-left ...
-        tl.x = max(0, tl.x)  # according to ...
-        tl.x = min(w2, tl.x)  # arrow key ...
-        tl.y += zoom[2] * (h2 / 2)  # provided, but ...
-        tl.y = max(0, tl.y)  # stay within ...
-        tl.y = min(h2, tl.y)  # the page rect
-        clip = fitz.Rect(tl, tl.x + w2, tl.y + h2)
-        # clip rect is ready, now fill it
-        mat = mat_0 * fitz.Matrix(2, 2)  # zoom matrix
-        pix = dlist.get_pixmap(alpha=False, matrix=mat, clip=clip)
-    img = pix.tobytes("ppm")  # make PPM image from pixmap for tkinter, requires PyMuPDF version > 1.14.5
-    return img, clip.tl  # return image, clip position
+    sg.Popup("Cancelling:", "No file to view")
+    sys.exit("Cancelled: no file to view supplied")
 
 
-# ------------------------------------------------------------------------------
-# get physical screen dimension to determine the page image max size
-# ------------------------------------------------------------------------------
+image_elem = sg.Image(data=view.get_page_image())  # make image element
 
-# print("screen size = " + str(sg.Window.get_screen_size()))
-w, h = sg.Window.get_screen_size()
-max_width = w - 20
-max_height = h - 55
-max_size = (max_width, max_height)
-
-# ------------------------------------------------------------------------------
-
-def make_form_title():
-    """
-    Compose string for main window title.
-    """
-    return "Page %i of %i from file %s" % (cur_page + 1, page_count, fname)
-
-
-
-data, clip_pos = get_page(
-    cur_page,  # read first page
-    zoom=False,  # not zooming yet
-    max_size=max_size,  # image max dim
-)
-
-image_elem = sg.Image(data=data)  # make image element
-
-goto = sg.InputText(
-    str(cur_page + 1), size=(5, 1), do_not_clear=True, key="PageNumber"
-)  # for display & input
+##goto = sg.InputText(
+##    str(cur_page + 1), size=(5, 1), do_not_clear=True, key="PageNumber"
+##)  # for display & input
 
 layout = [  # the form layout
-##    [
-##        #sg.ReadFormButton("Next"),
-##        #sg.ReadFormButton("Prior"),
-##        sg.Text("Page:"),
-##        goto,
-##        sg.Text("(%i)" % page_count),
-##        sg.ReadFormButton("Zoom"),
-##        sg.Text("(toggle on/off, use arrows to navigate while zooming)"),
-##    ],
     [image_elem],
 ]
 
-#form = sg.FlexForm(
 form = sg.Window(
-    title=make_form_title(),
+    title=view.get_view_title(),
     layout=layout,
     return_keyboard_events=True,
     location=(0, 0),
     use_default_focus=False,
     finalize=True,
 )
-
-#form.Layout(layout)  # define the form
 
 # define keybindings not known to PySimpleGUI (key with modifier)
 form.bind('<Control-KeyPress-q>', "key-CTRL-Q")
@@ -232,75 +267,59 @@ def is_Right(btn):
     return btn.startswith("Right:")
 
 
-def is_Zoom(btn):
-    return btn.startswith("Zoom")
+def is_ZoomIn(btn):
+    return btn == '+'
 
+def is_ZoomOut(btn):
+    return btn == '-'
+
+def is_ZoomFit(btn):
+    return btn in ('f', 'F', '*')
+
+def is_Zoom100(btn):
+    return btn in ('0',) 
 
 def is_MyKeys(btn):
-    return any((is_Next(btn), is_Prior(btn), is_Zoom(btn), is_Open(btn)))
+    return any((is_Next(btn), is_Prior(btn), is_ZoomIn(btn), is_ZoomOut(btn), is_Open(btn)))
 
 
 # old page store and zoom toggle
 old_page = 0
-old_zoom = False
-zoom_active = False
 
 while True:
     btn, value = form.Read()
-    #if btn is None and (value is None or value["PageNumber"] is None):
-    #    break
+
     if is_Quit(btn):
         with open(config_file_name, "w") as write_file:
-            config["recent_file"] = {"file_name": fname, "page": cur_page}
+            config["recent_file"] = view.config_dictionary()
             json.dump(config, write_file)
         break
-    zoom_pressed = False
-    zoom = False
 
     if is_Open(btn):
         fname = get_filename_from_GUI()
         if not fname:
             sg.Popup("Cancelling:", "No filename supplied")
             sys.exit("Cancelled: no filename supplied")
-        doc = fitz.open(fname)
-        page_count = len(doc)
-        dlist_tab = [None] * page_count
-        cur_page = 0
+        view = DocumentView(fname, page_index=0, max_size=max_size)
     
     elif is_Next(btn):
-        cur_page += 1
+        view.next_page()
     elif is_Prior(btn):
-        cur_page -= 1
-    elif is_Up(btn) and zoom_active:
-        zoom = (clip_pos, 0, -1)
-    elif is_Down(btn) and zoom_active:
-        zoom = (clip_pos, 0, 1)
-    elif is_Left(btn) and zoom_active:
-        zoom = (clip_pos, -1, 0)
-    elif is_Right(btn) and zoom_active:
-        zoom = (clip_pos, 1, 0)
-    elif is_Zoom(btn):
-        zoom_pressed = True
-        if not zoom_active:
-            zoom = (clip_pos, 0, 0)
+        view.previous_page()
+    elif is_ZoomIn(btn):
+        view.zoom *= 1.25
+    elif is_ZoomOut(btn):
+        view.zoom /= 1.25
+    elif is_ZoomFit(btn):
+        view.zoom = view.get_fit_zoom()
+    elif is_Zoom100(btn):
+        view.zoom = 1.0
 
-    # sanitize page number
-    while cur_page >= page_count:  # wrap around
-        cur_page -= page_count
-    while cur_page < 0:  # pages < 0 are valid but look bad
-        cur_page += page_count
-
-    if zoom_pressed and zoom_active:
-        zoom = zoom_pressed = zoom_active = False
-
-    data, clip_pos = get_page(cur_page, zoom=zoom, max_size=max_size)
-    image_elem.Update(data=data)
-    old_page = cur_page
-    old_zoom = zoom
-    zoom_active = zoom_pressed or zoom
+    # update view
+    image_elem.Update(data=view.get_page_image())
 
     # update page number field
     if is_MyKeys(btn):
-        form.set_title(make_form_title())
+        form.set_title(view.get_view_title())
 
 form.close()
